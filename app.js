4acacb1f5fc318dbd23b2063c855c1cd829bdff4
Faizan Ansari <39953141+5zan-Code@users.noreply.github.com>
2022-07-03 01:55:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@5zan-Code 
+KeyurPatel0202
+/
+ride-share-app
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+ride-share-app/app.js /
+@KeyurPatel0202
+KeyurPatel0202 update connection changes (mongoose)
+Latest commit 0835472 now
+ History
+ 1 contributor
+58 lines (48 sloc)  1.45 KB
+
 const express = require('express');
 const app = express();
 const env = require('dotenv/config');
@@ -14,12 +42,16 @@ main().catch(err => console.log(err));
 
 async function main() {
   await mongoose
-    .connect(config.dbConfig.databaseUrl)
+    .connect(config.dbConfig.databaseUrl,{
+       useNewUrlParser: true, 
+       useUnifiedTopology: true,
+    })
     .then(() => {
       console.log("Mongodb connected");
     })
     .catch((error) => {
-      console.log({ error_database_connection: error.message });
+      console.log({ error_database_connection: error.message },
+        );
     });
 }
 
@@ -52,3 +84,18 @@ app.use((err, req, res, next)=>{
 const PORT = config.PORT || 3000;
 
 app.listen(PORT,() => console.log(`Server running on port ${PORT}`));
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+You have no unread notifications
